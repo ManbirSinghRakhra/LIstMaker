4424786076ce87617b1066970e86248fe97cca66
Manbir Rakhra <manbir.s.rakhra@gmail.com>
2019-10-30 08:15:08 +1000

    Adding Viewholder, Adapter, ViewHolderLayout file

---
+++ app/src/main/java/me/rakhra/listmaker/ListSelectionViewHolder.kt
@@ -0,0 +1,12 @@
+package me.rakhra.listmaker
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class ListSelectionViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+{
+    val listPosition = itemView.findViewById<TextView>(R.id.itemNumber)
+    val listTitle = itemView.findViewById<TextView>(R.id.itemString)
+}
